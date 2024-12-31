e1b2cf7ba6de73a662fa600570a4b9f95dd819ca
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2024-12-31 19:42:44 +0300

    new test model

--- techSquad/tSquad/admin.py
+++ techSquad/tSquad/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Incidences
 # Register your models here.
+
+class IncidencesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+
+admin.site.register(Incidences, IncidencesAdmin)
--- techSquad/tSquad/models.py
+++ techSquad/tSquad/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+class Incidences(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.PointField(max_length=20)
+    #objects = GeoManager()
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Incidences"
