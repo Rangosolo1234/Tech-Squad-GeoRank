7093da1f5c8c6c69a08645ca61e1d472d831c1f5
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-01-03 10:13:45 +0300

    Updated settings models and admin files

--- techSquad/tSquad/admin.py
+++ techSquad/tSquad/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from leaflet.admin import LeafletGeoAdmin
 from .models import Incidences
 # Register your models here.
 
-class IncidencesAdmin(admin.ModelAdmin):
+class IncidencesAdmin(LeafletGeoAdmin):
     list_display = ('name', 'location')
 
 admin.site.register(Incidences, IncidencesAdmin)
--- techSquad/techSquad/settings.py
+++ techSquad/techSquad/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'leaflet',
     'tSquad',
 ]
 
@@ -127,3 +128,43 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LEAFLET_CONFIG = {
+    'DEFAULT_CENTER': (6.0, 45.0),
+    'DEFAULT_ZOOM': 16,
+    'MIN_ZOOM': 3,
+    'MAX_ZOOM': 18,
+    'DEFAULT_PRECISION': 6,
+    'TILES': [
+        # OpenStreetMap Tiles
+        ('OpenStreetMap', 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            'attribution': '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            'maxZoom': 19
+        }),
+        # OpenTopoMap Tiles
+        ('OpenTopoMap', 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+            'attribution': 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="https://opentopomap.org">OpenTopoMap</a>',
+            'maxZoom': 17
+        }),
+        # ESRI Satellite Tiles
+        ('ESRI Satellite', 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+            'attribution': 'Tiles &copy; Esri &mdash; Source: Esri, Maxar, Earthstar Geographics, and the GIS User Community',
+            'maxZoom': 18
+        }),
+        # CartoDB Positron Tiles
+        ('CartoDB Positron', 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+            'attribution': '&copy; <a href="https://www.carto.com/">CARTO</a>',
+            'maxZoom': 20
+        }),
+        ('OSM France', 'https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+	'maxZoom': 20,
+	'attribution': '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }),
+    ('Alidade satellite','https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
+	'minZoom': 0,
+	'maxZoom': 20,
+	'attribution': '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	'ext': 'jpg'
+})
+    ]
+}
+
