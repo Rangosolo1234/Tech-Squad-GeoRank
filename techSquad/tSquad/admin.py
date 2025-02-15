d366a03cda09deca52360b31055fe94b9104157a
Solomon Kipkirui <112020502+Rangosolo1234@users.noreply.github.com>
2025-02-16 01:53:57 +0300

    Delete techSquad directory

--- techSquad/tSquad/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from leaflet.admin import LeafletGeoAdmin
-from .models import Incidences
-# Register your models here.
-
-class IncidencesAdmin(LeafletGeoAdmin):
-    list_display = ('name', 'location')
-
-admin.site.register(Incidences, IncidencesAdmin)
